3c56e3c7099e1f999239941831a1c88d686a6281
Somyung Yun <dbsthaud99@gmail.com>
2020-01-28 15:19:17 +0900

    Jan 21 GPS team work(for test code2)

---
+++ src/real_publisher_ctrl.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include "ros/ros.h"
+#include "ackermann_msgs/AckermannDriveStamped.h"
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "ctrl_publisher");
+	
+	ros::NodeHandle nh;
+	
+	//the second parameter to advertise() is the size of the message queue used for publishing messages.	
+	ros::Publisher ackermann_pub = nh.advertise<ackermann_msgs::AckermannDriveStamped>("ctrl_cmd", 10);
+
+	ros::Rate loop_rate(5);
+
+	ackermann_msgs::AckermannDriveStamped ackermann_data;
+	ackermann_data.drive.steering_angle = 0;
+	ackermann_data.drive.speed = 0;
+
+	int count = 4;
+
+	while(ros::ok()) {
+		ackermann_data.drive.steering_angle = count;
+		ackermann_data.drive.speed = count;
+
+		ackermann_pub.publish(ackermann_data);
+		ROS_INFO("-------------------");
+		ROS_INFO("STEERING ANGLE: %fdegree", ackermann_data.drive.steering_angle);
+		ROS_INFO("SPEED: %fm/s", ackermann_data.drive.speed);
+		
+		//if there is no while
+		//ros::spin(); 
+		ros::spinOnce();
+
+		loop_rate.sleep();
+		count+=count;
+	}
+	
+	return 0;
+}
---
+++ src/real_publisher_pose.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include "ros/ros.h"
+#include "ackermann_msgs/AckermannDriveStamped.h"
+#include <geometry_msgs/PoseStamped.h>
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "pose_publisher");
+	
+	ros::NodeHandle nh;
+	
+	//the second parameter to advertise() is the size of the message queue used for publishing messages.	
+	ros::Publisher pose_pub_ = nh.advertise<geometry_msgs::PoseStamped>("current_pose", 10);
+
+	ros::Rate loop_rate(5);
+
+
+
+	geometry_msgs::PoseStamped pose_data;
+	pose_data.pose.position.x = 302457.243910768;
+	pose_data.pose.position.y = 4123705.91286103;
+
+
+
+	while(ros::ok()) {
+		
+		pose_pub_.publish(pose_data);
+		ROS_INFO("-------------------");
+		ROS_INFO("POSE_X: %f", pose_data.pose.position.x);
+		ROS_INFO("POSE_Y: %f", pose_data.pose.position.y);
+
+		//if there is no while
+		//ros::spin(); 
+		ros::spinOnce();
+
+		loop_rate.sleep();
+	}
+	
+	return 0;
+}
